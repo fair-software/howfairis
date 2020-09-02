7de46ca1a726f2da463cbe57d1a4bbb61c2b5d64
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-09-02 16:53:40 +0200

    removed gitkeep files; added stub for checks

--- 1-repository/check_repository.py
+++ 1-repository/check_repository.py
@@ -1,1 +1,16 @@
-print('TODO')
+def first_check():
+    pass
+
+
+def second_check():
+    pass
+
+
+def main():
+    print('repository checks TODO')
+    first_check()
+    second_check()
+
+
+if __name__ == "__main__":
+    main()
