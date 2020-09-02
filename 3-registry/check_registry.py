b4388512a85681a5389b34c90410f2d7a3ec60ca
Edidiong Etuk <edidiongmichael08@gmail.com>
2020-09-02 14:17:44 +0100

    feat: Checks if Pypi text is in a file

--- 3-registry/check_registry.py
+++ 3-registry/check_registry.py
@@ -1,10 +1,14 @@
-def has_pypi_badge(readme_string):
-    print("checking for pypi")
-    return readme_string.lower().find("pypi") != -1
+def has_pypi_badge(readme_file):
+    with open(readme_file) as text:
+        for line in text:
+            if 'PyPi' in line or 'pypi' in line:
+                return "PyPi is found"
+
+    return "No PyPi"
 
 
 def main():
-    has_pypi_badge("some test string with the workd PyPI in it")
+    print(has_pypi_badge("some test string with the workd PyPI in it"))
 
 
 if __name__ == "__main__":
