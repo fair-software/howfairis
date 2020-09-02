37ef0cec0ec31c1cbe423fecb2f7f0e70f626279
Carlos Martinez <c.martinez@esciencecenter.nl>
2020-09-02 15:05:44 +0200

    Add place holder scripts

--- check.sh
+++ check.sh
@@ -1,7 +1,15 @@
 #! /bin/sh
 
-if [ "$INPUT_SWITCH" == "registry" ] ; then
+if [ "$INPUT_SWITCH" == "repository" ] ; then
+    /usr/local/bin/python /data/1-repository/check_repository.py
+elif [ "$INPUT_SWITCH" == "license" ] ; then
+    /usr/local/bin/python /data/2-license/check_license.py
+elif [ "$INPUT_SWITCH" == "registry" ] ; then
     /usr/local/bin/python /data/3-registry/check_registry.py
+elif [ "$INPUT_SWITCH" == "citation" ] ; then
+    /usr/local/bin/python /data/4-citation/check_citation.py
+elif [ "$INPUT_SWITCH" == "checklist" ] ; then
+    /usr/local/bin/python /data/5-checklist/check_checklist.py
 else
     echo "Nope"
 fi
