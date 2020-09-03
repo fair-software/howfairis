68174d4d3eacf3ea9e320fd216b9eaa210e5b698
Edidiong Etuk <edidiongmichael08@gmail.com>
2020-09-03 19:09:09 +0100

    fixed reading from yaml in yaml_check function

--- howfairis/check_citation.py
+++ howfairis/check_citation.py
@@ -8,18 +8,18 @@ def file_check(filename):
     if path.isfile(filename):
         print ("File " +  filename + " found")
         return "File Found"
-    else: 
+    else:
         print("File " + filename + " not found")
         return "File Not Found"
 
 def YAML_check(filename):
     with open(filename) as file:
-    try:
-        citation = yaml.safe_load(file)
-        return "YAML file loaded"
-    except yaml.YAMLError as exc:
-            print(exc)
-        return "YAML failed to load"
+		try:
+			citation = yaml.safe_load(file)
+			return "YAML file loaded"
+		except yaml.YAMLError as exc:
+				print(exc)
+				return "YAML failed to load"
 
 
 
@@ -39,7 +39,7 @@ def Contents_check(filename):
     try:
         s(citation)
         print("Passed")
-        return "Citation Scheme passed"	
+        return "Citation Scheme passed"
     except	 MultipleInvalid as e:
         exc = e
         print ("Error: " + str(exc))
