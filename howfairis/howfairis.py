92bd4820e79d5106c8d12a42fc1ed3d99e35746b
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-09-03 17:54:44 +0200

    added method to calculate badge and see if that same badge is present

--- howfairis/howfairis.py
+++ howfairis/howfairis.py
@@ -1,4 +1,5 @@
-from howfairis.check_badge import main as check_badge
+import sys
+
 from howfairis.check_checklist import main as check_checklist
 from howfairis.check_citation import main as check_citation
 from howfairis.check_license import main as check_license
@@ -8,12 +9,12 @@ from howfairis.check_repository import main as check_repository
 
 class HowFairIsChecker:
     def __init__(self):
-        self.badge = None
         self.repository_is_compliant = None
         self.license_is_compliant = None
         self.registry_is_compliant = None
         self.citation_is_compliant = None
         self.checklist_is_compliant = None
+        self.badge = None
 
     def check_repository(self):
         self.repository_is_compliant = check_repository()
@@ -31,7 +32,36 @@ class HowFairIsChecker:
         self.checklist_is_compliant = check_checklist()
 
     def check_badge(self):
-        self.badge = check_badge()
+
+        compliance = [self.repository_is_compliant,
+                      self.license_is_compliant,
+                      self.registry_is_compliant,
+                      self.citation_is_compliant,
+                      self.checklist_is_compliant]
+
+        compliant_symbol = "%E2%97%8F"
+        noncompliant_symbol = "%E2%97%8B"
+        compliance_string = "%20".join([compliant_symbol if c is True else noncompliant_symbol for c in compliance])
+        score = compliance_string.count(compliant_symbol)
+        if score in [0, 1]:
+            color_string = "red"
+        elif score in [2, 3, 4]:
+            color_string = "orange"
+        elif score == 5:
+            color_string = "green"
+
+        self.badge = "https://img.shields.io/badge/fair--software.eu-{0}-{1}".format(compliance_string, color_string)
+
+        readme_string = "sdlfnsdnfsdnf https://img.shields.io/badge/fair--software.eu-%E2%97%8F%20%E2%97%8F%20" +\
+                        "%E2%97%8F%20%E2%97%8F%20%E2%97%8F-green a sdnfk anjsdfkj adnfkj   abnsdfibuweifkj xzk" +\
+                        "cijbsdi"
+
+        if readme_string.find(self.badge) == -1:
+            print("Expected badge is equal to the actual badge. It's all good.")
+            sys.exit(0)
+        else:
+            print("Expected badge value:\n" + self.badge + "\n")
+            sys.exit(1)
 
 
 def main():
