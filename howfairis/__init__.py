2219163853e3d58303a3f9a39874641246cc745b
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-09-04 17:04:41 +0200

    cookiecutter files merged with existing content

--- howfairis/__init__.py
+++ howfairis/__init__.py
@@ -1,17 +1,5 @@
-from howfairis.__version__ import __version__
-from howfairis.check_badge import main as check_badge
-from howfairis.check_checklist import main as check_checklist
-from howfairis.check_license import main as check_license
-from howfairis.check_registry import main as check_registry
-from howfairis.check_repository import main as check_repository
-from howfairis.howfairis import main as howfairis
+from .__version__ import __version__
 
-__all__ = [
-    "__version__",
-    "check_badge",
-    "check_checklist",
-    "check_license",
-    "check_registry",
-    "check_repository",
-    "howfairis"
-]
+
+__author__ = "https://github.com/jspaaks"
+__email__ = 'j.spaaks@esciencecenter.nl'
--- setup.py
+++ setup.py
@@ -1,28 +1,61 @@
-#!/usr/bin/env python
 import os
-from setuptools import find_packages
+
 from setuptools import setup
 
+
 here = os.path.abspath(os.path.dirname(__file__))
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
 
 setup(
     name="howfairis",
     entry_points={
-        "console_scripts": ["howfairis=howfairis.howfairis:main"],
+        "console_scripts": ["howfairis=howfairis:main"],
     },
     version="0.1.0",
-    packages=find_packages(exclude=['*tests*']),
+    description="Python package to analyze compliance with fair-software.eu recommendations",
+    long_description=readme + "\n\n",
+    author="https://github.com/jspaaks",
+    author_email="j.spaaks@esciencecenter.nl",
+    url="https://github.com/fair-software/howfairis",
+    packages=[
+        "howfairis",
+    ],
     include_package_data=True,
     license="Apache Software License 2.0",
+    zip_safe=False,
+    keywords="howfairis",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    test_suite="tests",
     install_requires=[
-        "pyyaml>=5.3"
+        "requests"
     ],
     setup_requires=[
+        # dependency for `python setup.py test`
+        "pytest-runner"
     ],
     tests_require=[
+        "pytest",
+        "pytest-cov",
+        "pycodestyle",
     ],
     extras_require={
+        "dev":  [
+            "prospector[with_pyroma]",
+            "yapf",
+            "isort",
+            "bumpversion"
+        ],
     },
-    package_data={"howfairis": ["registries.yaml"]},
+    data_files=[]
 )
-
