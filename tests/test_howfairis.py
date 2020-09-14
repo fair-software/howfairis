b50047479e3b1fc735bba1f9494b0741fd96784f
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-09-14 21:06:32 +0200

    added heavy handed live test for rsd repos

--- tests/test_howfairis.py
+++ tests/test_howfairis.py
@@ -3,24 +3,27 @@
 
 """Tests for the howfairis module.
 """
-import pytest
+import requests
+import howfairis
 
 
-def test_something():
-    assert True
+def test_heavy_handed_livetest_rsd():
+    software_api_url = "https://www.research-software.nl/api/software?isPublished=true"
+    try:
+        response = requests.get(software_api_url)
+        # If the response was successful, no Exception will be raised
+        response.raise_for_status()
+    except requests.HTTPError:
+        print("Unable to retrieve the list of URLs")
+        return False
 
+    urls = []
+    for d in response.json():
+        for key, values in d["repositoryURLs"].items():
+            urls.extend(values)
 
-def test_with_error():
-    with pytest.raises(ValueError):
-        # Do something that raises a ValueError
-        raise(ValueError)
-
-
-# Fixture example
-@pytest.fixture
-def an_object():
-    return {}
-
-
-def test_howfairis(an_object):
-    assert an_object == {}
+    for url in urls[20:]:
+        print(url)
+        checker = howfairis.HowFairIsChecker(url).check_five_recommendations()
+        for c in checker.compliance:
+            assert isinstance(c, bool)
