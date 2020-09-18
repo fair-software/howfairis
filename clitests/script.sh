a4c35ea77e64c2f05037023b647dd26393345abe
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-09-18 13:55:43 +0200

    updated cli test calls

--- clitests/script.sh
+++ clitests/script.sh
@@ -7,10 +7,10 @@ which howfairis
 howfairis --help
 howfairis --version
 howfairis https://github.com/fair-software/badge-test
-howfairis https://github.com/fair-software/badge-test -p force/00100
-howfairis https://github.com/fair-software/badge-test -p force/10110
-howfairis https://github.com/fair-software/badge-test -p force/11110
-howfairis https://github.com/fair-software/badge-test -p force/11111
-howfairis https://github.com/fair-software/badge-test -p force/uu1uu
-howfairis https://github.com/fair-software/badge-test -p force/u
-howfairis https://github.com/fair-software/badge-test -p include_comments
+howfairis https://github.com/fair-software/badge-test -p /force/00100
+howfairis https://github.com/fair-software/badge-test -p /force/10110
+howfairis https://github.com/fair-software/badge-test -p /force/11110
+howfairis https://github.com/fair-software/badge-test -p /force/11111
+howfairis https://github.com/fair-software/badge-test -p /force/uu1uu
+howfairis https://github.com/fair-software/badge-test -p /force/u
+howfairis https://github.com/fair-software/badge-test -p /include_comments
