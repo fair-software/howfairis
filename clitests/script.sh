49a05812cdd65057b9edc8206e9c03d7050dd36a
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-09-18 14:28:03 +0200

    added cli tests to badge-test repo on gitlab

--- clitests/script.sh
+++ clitests/script.sh
@@ -6,6 +6,8 @@ set -e
 which howfairis
 howfairis --help
 howfairis --version
+
+# github
 howfairis https://github.com/fair-software/badge-test
 howfairis https://github.com/fair-software/badge-test -p /force/00100
 howfairis https://github.com/fair-software/badge-test -p /force/10110
@@ -14,3 +16,14 @@ howfairis https://github.com/fair-software/badge-test -p /force/11111
 howfairis https://github.com/fair-software/badge-test -p /force/uu1uu
 howfairis https://github.com/fair-software/badge-test -p /force/u
 howfairis https://github.com/fair-software/badge-test -p /include_comments
+
+
+# gitlab
+howfairis https://gitlab.com/jspaaks/badge-test
+howfairis https://gitlab.com/jspaaks/badge-test -p /force/00100
+howfairis https://gitlab.com/jspaaks/badge-test -p /force/10110
+howfairis https://gitlab.com/jspaaks/badge-test -p /force/11110
+howfairis https://gitlab.com/jspaaks/badge-test -p /force/11111
+howfairis https://gitlab.com/jspaaks/badge-test -p /force/uu1uu
+howfairis https://gitlab.com/jspaaks/badge-test -p /force/u
+howfairis https://gitlab.com/jspaaks/badge-test -p /include_comments
