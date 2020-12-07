4a97526366f10b2bea41c94ff785d2b2e635a84c
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-12-07 16:07:29 +0100

    added exception for pylint error

--- howfairis/cli.py
+++ howfairis/cli.py
@@ -71,6 +71,7 @@ def check_badge(compliance, readme=None):
 @click.argument("url", required=False)
 def cli(url=None, branch=None, config_file=None, remote_config_file=None, path=None,
         show_trace=False, version=False, ignore_remote_config=False, show_default_config=False):
+    # pylint: disable=too-many-locals
 
     """Determine compliance with recommendations from fair-software.eu for the GitHub or GitLab repository at URL."""
 
