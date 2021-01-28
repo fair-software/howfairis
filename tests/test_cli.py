72ffbf4029b2a999215465233bfb329f5b5480e9
Erik Tjong Kim Sang <erikt@xs4all.nl>
2021-01-28 18:35:18 +0100

    fixed linter complaints

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,13 +3,6 @@ from click.testing import CliRunner, Result
 from howfairis import cli
 
 
-def test_valid_url():
-    runner = CliRunner()
-    result = runner.invoke(cli.cli, ["https://github.com/fair-software/howfairis"])
-    expected_exit_code = 0
-    expected_exception = None
-    assert str(result.exception) == str(expected_exception)
-
 def test_invalid_url():
     runner = CliRunner()
     result = runner.invoke(cli.cli, ["howfairis"])
