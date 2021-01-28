502e1b72841c5069de861f53bda4d2ba3c0db0fb
Erik Tjong Kim Sang <erikt@xs4all.nl>
2021-01-28 18:08:15 +0100

    automatic url testing

---
+++ tests/test_cli.py
@@ -0,0 +1,29 @@
+import pytest
+from click.testing import CliRunner, Result
+from howfairis import cli
+
+
+def test_valid_url():
+    runner = CliRunner()
+    result = runner.invoke(cli.cli, ["https://github.com/fair-software/howfairis"])
+    expected_exit_code = 0
+    assert result.exit_code == expected_exit_code
+
+def test_invalid_url():
+    runner = CliRunner()
+    result = runner.invoke(cli.cli, ["howfairis"])
+    expected_exception = "url should start with https://"
+    assert str(result.exception) == expected_exception
+
+def test_no_github():
+    runner = CliRunner()
+    result = runner.invoke(cli.cli, ["https://www.esciencecenter.nl"])
+    expected_exception = "Repository should be on github.com or on gitlab.com"
+    assert str(result.exception) == expected_exception
+
+def test_no_repository():
+    runner = CliRunner()
+    result = runner.invoke(cli.cli, ["https://github.com/fair-software"])
+    expected_exception = "url is not a repository"
+    assert str(result.exception) == expected_exception
+
