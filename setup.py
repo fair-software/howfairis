41952654f82d4611a1d0c3650fb3560f8e699d61
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2021-01-29 11:21:46 +0100

    Drop Python 3.5 support + Added Python 3.8 and 3.9 support
    Fixes #70

--- setup.py
+++ setup.py
@@ -29,9 +29,10 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     test_suite="tests",
     install_requires=[
