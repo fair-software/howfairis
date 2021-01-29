bbca05b9f80a81f618c5c55fa65044a876b7f474
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2021-01-29 11:02:29 +0100

    Update tests/test_config.py
    Co-authored-by: Faruk D. <fdiblen@users.noreply.github.com>

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,7 +3,7 @@ from requests_mock import Mocker
 from howfairis import Config, Repo
 
 
-def test_default(requests_mock: Mocker):
+def test_default_config(requests_mock: Mocker):
     requests_mock.get('https://raw.githubusercontent.com/fair-software/does-not-exist/master/.howfairis.yml', status_code=404)
     repo = Repo('https://github.com/fair-software/does-not-exist')
     config = Config(repo)
