371d62aa2e834c841eb6f95a754b9346ad446d6d
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-02-03 23:26:42 +0100

    added tests methods for RepoTest

--- tests/github/fair_software/badge/RepoTest.py
+++ tests/github/fair_software/badge/RepoTest.py
@@ -1,36 +1,43 @@
 from tests.contracts.Repo import Contract
-from howfairis import Repo
 import unittest
-import requests_mock
-from tests.github.fair_software.badge.mocks import get_mocked_responses
+import pytest
+from tests.github.fair_software.badge.mocks import mocked_repo
+from howfairis import Platform
 
 
 class RepoTest(Contract, unittest.TestCase):
 
+    @pytest.fixture(autouse=True)
+    def __inject_fixtures(self, mocked_repo):
+        self.mocked_repo = mocked_repo
+
     def test_api(self):
-        with requests_mock.Mocker() as mocker:
-            for args, kwargs in get_mocked_responses():
-                mocker.get(*args, **kwargs)
-            actual_repo = Repo("https://github.com/fair-software/badge")
-            assert actual_repo.api == "https://api.github.com/repos/fair-software/badge", "TODO"
+        assert self.mocked_repo.api == "https://api.github.com/repos/fair-software/badge"
 
     def test_branch(self):
-        with requests_mock.Mocker() as mocker:
-            for args, kwargs in get_mocked_responses():
-                mocker.get(*args, **kwargs)
-            actual_repo = Repo("https://github.com/fair-software/badge")
-            assert actual_repo.branch is None, "TODO"
+        assert self.mocked_repo.branch is None
+
+    def test_config_file(self):
+        assert self.mocked_repo.config_file is None
 
     def test_default_branch(self):
-        with requests_mock.Mocker() as mocker:
-            for args, kwargs in get_mocked_responses():
-                mocker.get(*args, **kwargs)
-            actual_repo = Repo("https://github.com/fair-software/badge")
-            assert actual_repo.default_branch == "master", "TODO"
+        assert self.mocked_repo.default_branch == "master"
+
+    def test_owner(self):
+        assert self.mocked_repo.owner == "fair-software"
+
+    def test_path(self):
+        assert self.mocked_repo.path == ""
+
+    def test_platform(self):
+        assert self.mocked_repo.platform == Platform.GITHUB
+
+    def test_raw_url_format_string(self):
+        assert self.mocked_repo.raw_url_format_string == "https://raw.githubusercontent.com/fair-software" + \
+                                                         "/badge/master/{0}"
+
+    def test_repo(self):
+        assert self.mocked_repo.repo == "badge"
 
     def test_url(self):
-        with requests_mock.Mocker() as mocker:
-            for args, kwargs in get_mocked_responses():
-                mocker.get(*args, **kwargs)
-            actual_repo = Repo("https://github.com/fair-software/badge")
-            assert actual_repo.url == "https://github.com/fair-software/badge", "TODO"
+        assert self.mocked_repo.url == "https://github.com/fair-software/badge"
