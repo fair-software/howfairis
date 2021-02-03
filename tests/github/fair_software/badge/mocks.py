52b12bffc884a53221059512875d984431437b90
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-02-03 23:26:10 +0100

    added mocked behavior for fair-software/badge repo

--- tests/github/fair_software/badge/mocks.py
+++ tests/github/fair_software/badge/mocks.py
@@ -1,5 +1,15 @@
-def get_mocked_responses():
-    return [
-        (("http://github.com/fair-software/badge", ), dict(text="dummy response")),
-        (("https://api.github.com/repos/fair-software/badge", ), dict(json=dict(default_branch="master")))
-    ]
+import requests_mock
+import pytest
+from howfairis import Repo
+
+
+@pytest.fixture
+def mocked_repo():
+    """This mock aims to reflect the state of the repository at
+    https://github.com/fair-software/badge/tree/b3f90ec9c2b1be604f482c2d9e46a9aeca3ee45a"""
+    with requests_mock.Mocker() as mocker:
+        mocker.get("http://github.com/fair-software/badge", text="dummy response")
+        mocker.get("https://api.github.com/repos/fair-software/badge", json=dict(default_branch="master"))
+        return Repo("https://github.com/fair-software/badge")
+
+
