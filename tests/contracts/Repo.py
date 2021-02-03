ec8294b619898ac2003579fb06e9258452c44466
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-02-03 23:25:18 +0100

    added simple methods to the testing contract for testing the members of the Repo class

--- tests/contracts/Repo.py
+++ tests/contracts/Repo.py
@@ -12,9 +12,34 @@ class Contract(ABC):
         pass
 
     @abstractmethod
+    def test_config_file(self):
+        pass
+
+    @abstractmethod
     def test_default_branch(self):
+        pass
+
+    @abstractmethod
+    def test_owner(self):
+        pass
+
+    @abstractmethod
+    def test_path(self):
+        pass
+
+    @abstractmethod
+    def test_platform(self):
+        pass
+
+    @abstractmethod
+    def test_raw_url_format_string(self):
+        pass
+
+    @abstractmethod
+    def test_repo(self):
         pass
 
     @abstractmethod
     def test_url(self):
         pass
+
