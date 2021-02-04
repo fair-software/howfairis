b4aa0f0c9688e38fc8f16eac33fae32e2b99541a
Erik Tjong Kim Sang <erikt@xs4all.nl>
2021-02-04 19:01:19 +0100

    fixed linter complaints

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
         "beautifulsoup4>=4",
         "click>=7",
         "colorama>=0",
+        "python-dateutil>=0",
         "ruamel.yaml>=0.16",
         "requests>=2",
         "voluptuous>=0.11"
