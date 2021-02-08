61eb02617280b9cbb7a6bb0555260f21a1693fa9
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2021-02-08 14:10:19 +0100

    Update tests/gitlab/jspaaks/badge_test/mocker.py

--- tests/gitlab/jspaaks/badge_test/mocker.py
+++ tests/gitlab/jspaaks/badge_test/mocker.py
@@ -6,8 +6,8 @@ import requests_mock
 def mocker():
     """This mock aims to reflect the state of the repository at
     https://gitlab.com/jspaaks/badge-test/-/tree/472713282fde87fa4c8e5116e46229c094c3c4ec"""
-    with requests_mock.Mocker() as mocker:
-        mocker.get("https://gitlab.com/jspaaks/badge-test")
-        mocker.get("https://gitlab.com/api/v4/projects/jspaaks%2Fbadge-test", json=dict(default_branch="master"))
-        mocker.get("https://gitlab.com/jspaaks/badge-test/-/raw/master/.howfairis.yml", status_code=404)
-        return mocker
+    with requests_mock.Mocker() as m:
+        m.get("https://gitlab.com/jspaaks/badge-test")
+        m.get("https://gitlab.com/api/v4/projects/jspaaks%2Fbadge-test", json=dict(default_branch="master"))
+        m.get("https://gitlab.com/jspaaks/badge-test/-/raw/master/.howfairis.yml", status_code=404)
+        return m
