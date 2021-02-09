577f92083b79a0295995f16fa9b417c44564c53a
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-02-09 15:56:29 +0100

    sorted requests and shortened var names for repo, raw, and api urls

--- tests/github/fair_software/badge/mocker.py
+++ tests/github/fair_software/badge/mocker.py
@@ -16,14 +16,14 @@ def mocker():
     default_branch_response = {"default_branch": "master"}
 
     with requests_mock.Mocker() as m:
-        repo_url, raw_url, api_url = get_urls(Platform.GITHUB, owner="fair-software", repo="badge")
-        m.get(repo_url, status_code=200)
-        m.get(api_url + "/license", status_code=200)
-        m.get(api_url, status_code=200, json=default_branch_response)
-        m.get(raw_url + "/master/.howfairis.yml", status_code=404)
-        m.get(raw_url + "/master/README.rst", status_code=404)
-        m.get(raw_url + "/master/README.md", status_code=200, text=readme_text)
-        m.get(raw_url + "/master/CITATION", status_code=404)
-        m.get(raw_url + "/master/CITATION.cff", status_code=200, text=cff_text)
-        m.get(raw_url + "/master/.zenodo.json", status_code=404)
+        repo, raw, api = get_urls(Platform.GITHUB, owner="fair-software", repo="badge")
+        m.get(repo, status_code=200)
+        m.get(raw + "/master/.howfairis.yml", status_code=404)
+        m.get(raw + "/master/.zenodo.json", status_code=404)
+        m.get(raw + "/master/CITATION.cff", status_code=200, text=cff_text)
+        m.get(raw + "/master/CITATION", status_code=404)
+        m.get(raw + "/master/README.md", status_code=200, text=readme_text)
+        m.get(raw + "/master/README.rst", status_code=404)
+        m.get(api + "/license", status_code=200)
+        m.get(api, status_code=200, json=default_branch_response)
         return m
