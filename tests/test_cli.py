04c3120ef1f8b9c86fdfc689f7ba6e71c028f229
Erik Tjong Kim Sang <erikt@xs4all.nl>
2021-02-09 17:17:46 +0100

    more tests for check

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,8 +1,50 @@
+from requests_mock import Mocker
+
 from howfairis.cli import github_readme_creation_check
 from howfairis.vcs_platform import Platform
-
+from datetime import datetime
+from datetime import timedelta
+from dateutil import tz
 
 def test_github_readme_creation_check_platform():
     actual_return_code = github_readme_creation_check("", "", Platform.GITLAB, None)
     expected_return_code = 0
     assert actual_return_code == expected_return_code
+
+def test_github_readme_creation_check_critical_time(requests_mock: Mocker):
+    url = "https://github.com/fair-software/howfairis"
+    filename = "README.rst"
+    platform = Platform.GITHUB
+    branch = None
+    date_now = datetime.now().astimezone(tz.tzutc())
+    date_string = date_now.strftime("%Y-%m-%dT%H:%M:%SZ")
+    requests_mock.get(url+"/blob/master/"+filename,
+                      text=f'<relative-time datetime="{date_string}">')
+    actual_return_code = github_readme_creation_check(url, filename, Platform.GITHUB, branch)
+    expected_return_code = 1
+    assert actual_return_code == expected_return_code
+
+def test_github_readme_creation_check_fine_time(requests_mock: Mocker):
+    url = "https://github.com/fair-software/howfairis"
+    filename = "README.rst"
+    platform = Platform.GITHUB
+    branch = None
+    date_now = datetime.now().astimezone(tz.tzutc())
+    date_string = (date_now-timedelta(minutes=10)).strftime("%Y-%m-%dT%H:%M:%SZ")
+    requests_mock.get(url+"/blob/master/"+filename,
+                      text=f'<relative-time datetime="{date_string}">')
+    actual_return_code = github_readme_creation_check(url, filename, Platform.GITHUB, branch)
+    expected_return_code = 0
+    assert actual_return_code == expected_return_code
+ 
+def test_github_readme_creation_check_no_times_available(requests_mock: Mocker):
+    url = "https://github.com/fair-software/howfairis"
+    filename = "README.rst"
+    platform = Platform.GITHUB
+    branch = None
+    requests_mock.get(url+"/blob/master/"+filename, text="there is no time here")
+    requests_mock.get(url+"/contributors/master/"+filename, text="there is no time here")
+    actual_return_code = github_readme_creation_check(url, filename, Platform.GITHUB, branch)
+    expected_return_code = 0
+    assert actual_return_code == expected_return_code
+
