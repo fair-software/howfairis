9de9683d75427bc34f60f6f08cb8e9eaa9182849
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-02-10 18:20:23 +0100

    gitlab's Config, Checker, Repo tests now use mocked context manager from conftest.py

--- tests/gitlab/jspaaks/badge_test/conftest.py
+++ tests/gitlab/jspaaks/badge_test/conftest.py
@@ -12,17 +12,21 @@ def mocker() -> Mocker:
     """This mock aims to reflect the state of the repository at
     https://gitlab.com/jspaaks/badge-test/-/tree/472713282fde87fa4c8e5116e46229c094c3c4ec"""
 
+    default_branch_response = {"default_branch": "master"}
+    index_html_text = load_file_from_local_data("index.html", __file__)
+    readme_text = load_file_from_local_data("README.md", __file__)
+
     with requests_mock.Mocker() as m:
         repo, raw, api = get_urls(Platform.GITLAB, owner="jspaaks", repo="badge-test")
 
-        # m.get(repo, status_code=200, text=index_html_text)
-        # m.get(raw + "/master/.howfairis.yml", status_code=404)
-        # m.get(raw + "/master/.zenodo.json", status_code=404)
-        # m.get(raw + "/master/codemeta.json", status_code=404)
-        # m.get(raw + "/master/CITATION.cff", status_code=200, text=cff_text)
-        # m.get(raw + "/master/CITATION", status_code=404)
-        # m.get(raw + "/master/README.md", status_code=200, text=readme_text)
-        # m.get(raw + "/master/README.rst", status_code=404)
-        # m.get(api + "/license", status_code=200)
-        # m.get(api, status_code=200, json=default_branch_response)
+        m.get(repo, status_code=200, text=index_html_text)
+        m.get(raw + "/master/.howfairis.yml", status_code=404)
+        m.get(raw + "/master/.zenodo.json", status_code=404)
+        m.get(raw + "/master/codemeta.json", status_code=404)
+        m.get(raw + "/master/CITATION.cff", status_code=404)
+        m.get(raw + "/master/CITATION", status_code=404)
+        m.get(raw + "/master/README.md", status_code=200, text=readme_text)
+        m.get(raw + "/master/README.rst", status_code=404)
+        m.get(api + "/repository/tree", status_code=200)
+        m.get(api, status_code=200, json=default_branch_response)
         return m
