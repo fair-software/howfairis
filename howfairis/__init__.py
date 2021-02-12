6d5872eff810136d56414b661acfdabb0d0676a8
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-02-12 10:08:46 +0100

    sphinxdoc wasnt aware of submodule, maybe thisll fix it

--- howfairis/__init__.py
+++ howfairis/__init__.py
@@ -1,9 +1,8 @@
+from . import exceptions
 from .__version__ import __version__
 from .checker import Checker
 from .code_repository_platforms import Platform
 from .compliance import Compliance
-from .exceptions.GetDefaultBranchException import GetDefaultBranchException
-from .exceptions.HowfairisException import HowfairisException
 from .repo import Repo
 
 
@@ -11,11 +10,11 @@ __author__ = "https://github.com/jspaaks"
 __email__ = 'j.spaaks@esciencecenter.nl'
 
 __all__ = [
+    "exceptions",
     "__version__",
     "Checker",
     "Compliance",
-    "GetDefaultBranchException",
-    "HowfairisException",
     "Platform",
     "Repo"
+
 ]
