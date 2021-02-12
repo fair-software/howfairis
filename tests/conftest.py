53021555265c06ac00ee06f7688ee730b022c218
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-02-12 16:15:51 +0100

    added automatic test annotation with pytest.marks

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+
+
+def pytest_collection_modifyitems(items):
+    for item in items:
+        if "bitbucket" in item.nodeid:
+            item.add_marker(pytest.mark.bitbucket)
+        if "heptapod" in item.nodeid:
+            item.add_marker(pytest.mark.heptapod)
+        if "github" in item.nodeid:
+            item.add_marker(pytest.mark.github)
+        if "gitlab" in item.nodeid:
+            item.add_marker(pytest.mark.gitlab)
+        if item.nodeid.split("::")[1].startswith("TestRepo"):
+            item.add_marker(pytest.mark.repo)
+        if item.nodeid.split("::")[1].startswith("TestChecker"):
+            item.add_marker(pytest.mark.checker)
+        if item.nodeid.split("::")[1].startswith("TestCompliance"):
+            item.add_marker(pytest.mark.compliance)
+
