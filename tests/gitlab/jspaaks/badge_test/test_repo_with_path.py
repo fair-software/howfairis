d4b05f84976bde650170f4a446772ea70f633788
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-02-12 15:55:26 +0100

    added mocked tests for gitlab

---
+++ tests/gitlab/jspaaks/badge_test/test_repo_with_path.py
@@ -0,0 +1,42 @@
+import pytest
+from howfairis import Platform
+from howfairis import Repo
+from tests.contracts.repo import Contract
+from .mocker import mocker
+
+
+@pytest.fixture
+def mocked_repo(mocker):
+    with mocker:
+        return Repo("https://gitlab.com/jspaaks/badge-test", path="mypath")
+
+
+class TestRepoWithPath(Contract):
+
+    def test_api(self, mocked_repo):
+        assert mocked_repo.api == "https://gitlab.com/api/v4/projects/jspaaks%2Fbadge-test"
+
+    def test_branch(self, mocked_repo):
+        assert mocked_repo.branch is None
+
+    def test_default_branch(self, mocked_repo):
+        assert mocked_repo.default_branch == "master"
+
+    def test_owner(self, mocked_repo):
+        assert mocked_repo.owner == "jspaaks"
+
+    def test_path(self, mocked_repo):
+        assert mocked_repo.path == "/mypath"
+
+    def test_platform(self, mocked_repo):
+        assert mocked_repo.platform == Platform.GITLAB
+
+    def test_raw_url_format_string(self, mocked_repo):
+        assert mocked_repo.raw_url_format_string == \
+            "https://gitlab.com/jspaaks/badge-test/-/raw/master/mypath/{0}"
+
+    def test_repo(self, mocked_repo):
+        assert mocked_repo.repo == "badge-test"
+
+    def test_url(self, mocked_repo):
+        assert mocked_repo.url == "https://gitlab.com/jspaaks/badge-test"
