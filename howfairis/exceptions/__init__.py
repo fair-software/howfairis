8c7c5028586b9418d35bcaa848b4693429db6089
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-02-12 13:14:14 +0100

    avoid circular dependencies

--- howfairis/exceptions/__init__.py
+++ howfairis/exceptions/__init__.py
@@ -1,5 +1,5 @@
-from .get_default_branch_exception import GetDefaultBranchException
-from .howfairis_exception import HowfairisException
+from howfairis.exceptions.get_default_branch_exception import GetDefaultBranchException
+from howfairis.exceptions.howfairis_exception import HowfairisException
 
 
 __all__ = [
