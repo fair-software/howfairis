be026f562ca5e47907beca4229d8103893f98889
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-02-15 17:17:00 +0100

    Update tests/conftest.py
    Co-authored-by: Faruk D. <fdiblen@users.noreply.github.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,10 +3,6 @@ import pytest
 
 def pytest_collection_modifyitems(items):
     for item in items:
-        if "bitbucket" in item.nodeid:
-            item.add_marker(pytest.mark.bitbucket)
-        if "heptapod" in item.nodeid:
-            item.add_marker(pytest.mark.heptapod)
         if "github" in item.nodeid:
             item.add_marker(pytest.mark.github)
         if "gitlab" in item.nodeid:
@@ -17,4 +13,3 @@ def pytest_collection_modifyitems(items):
             item.add_marker(pytest.mark.checker)
         if item.nodeid.split("::")[1].startswith("TestCompliance"):
             item.add_marker(pytest.mark.compliance)
-
