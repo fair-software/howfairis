d16d58bb0d83de77898a106aa0a0e8ba61454357
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-02-15 18:11:55 +0100

    trying to build api docs

--- docs/conf.py
+++ docs/conf.py
@@ -124,7 +124,7 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_context = {
+html_css_files = {
     'css_files': [
         '_static/theme_overrides.css',  # override wide tables in RTD theme
     ],
