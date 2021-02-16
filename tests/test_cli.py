1a189053d55285277e43c86cd7d9047dfe5652d8
Erik Tjong Kim Sang <erikt@xs4all.nl>
2021-02-16 15:10:14 +0100

    basic cli tests

---
+++ tests/test_cli.py
@@ -0,0 +1,26 @@
+from click.testing import CliRunner
+from howfairis.cli import cli
+
+
+runner = CliRunner()
+
+
+def test_invalid_url():
+    runner = CliRunner()
+    response = runner.invoke(cli, [""])
+    expected_message = "url should start with https://"
+    assert response.exception.args[0] == expected_message
+
+
+def test_url_not_git():
+    runner = CliRunner()
+    response = runner.invoke(cli, ["https://www.esciencecenter.nl"])
+    expected_message = "Repository should be on github.com or on gitlab.com."
+    assert response.exception.args[0] == expected_message
+
+
+def test_url_not_repository():
+    runner = CliRunner()
+    response = runner.invoke(cli, ["https://github.com/fair-software"])
+    expected_message = "url is not a repository"
+    assert response.exception.args[0] == expected_message
