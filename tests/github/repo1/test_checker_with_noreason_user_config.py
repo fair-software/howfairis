ff220d247355c2298d1b8c6a3d5b8b9a6a8f48c4
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2021-02-16 17:38:16 +0100

    Copy unchanged tests from test_checker_no_args.py

--- tests/github/repo1/test_checker_with_noreason_user_config.py
+++ tests/github/repo1/test_checker_with_noreason_user_config.py
@@ -1,12 +1,13 @@
 import pytest
 from requests_mock import Mocker
-from howfairis import Checker
+from howfairis import Checker, Compliance
 from howfairis import Repo
+from howfairis.readme import Readme
 from tests.contracts.checker import Contract
 from tests.helpers.load_files_from_local_data import list_files_from_local_data
 
 
-def get_checker(user_config_filename):
+def get_checker(user_config_filename=None):
     repo = Repo("https://github.com/fair-software/badge")
     return Checker(repo, user_config_filename=user_config_filename)
 
@@ -53,65 +54,109 @@ class TestCheckerWithReasonableUserConfig(Contract):
             captured = capsys.readouterr()
             assert "no reason provided" in captured.out
 
-    def test_compliance(self, mocker: Mocker):
-        pass
+    def test_compliance(self, mocker):
+        with mocker:
+            mocked_checker = get_checker()
+            actual_compliance = mocked_checker.check_five_recommendations()
+            expected_compliance = Compliance(True, True, True, True, False)
+            assert actual_compliance == expected_compliance
 
-    def test_has_ascl_badge(self, mocker: Mocker):
-        pass
+    def test_has_ascl_badge(self, mocker):
+        with mocker:
+            mocked_checker = get_checker()
+            assert mocked_checker.has_ascl_badge() is False
 
-    def test_has_bintray_badge(self, mocker: Mocker):
-        pass
+    def test_has_bintray_badge(self, mocker):
+        with mocker:
+            mocked_checker = get_checker()
+            assert mocked_checker.has_bintray_badge() is False
 
-    def test_has_citation_file(self, mocker: Mocker):
-        pass
+    def test_has_citation_file(self, mocker):
+        with mocker:
+            mocked_checker = get_checker()
+            assert mocked_checker.has_citation_file() is False
 
-    def test_has_citationcff_file(self, mocker: Mocker):
-        pass
+    def test_has_citationcff_file(self, mocker):
+        with mocker:
+            mocked_checker = get_checker()
+            assert mocked_checker.has_citationcff_file() is True
 
-    def test_has_codemeta_file(self, mocker: Mocker):
-        pass
+    def test_has_codemeta_file(self, mocker):
+        with mocker:
+            mocked_checker = get_checker()
+            assert mocked_checker.has_codemeta_file() is False
 
-    def test_has_conda_badge(self, mocker: Mocker):
-        pass
+    def test_has_conda_badge(self, mocker):
+        with mocker:
+            mocked_checker = get_checker()
+            assert mocked_checker.has_conda_badge() is False
 
-    def test_has_core_infrastructures_badge(self, mocker: Mocker):
-        pass
+    def test_has_core_infrastructures_badge(self, mocker):
+        with mocker:
+            mocked_checker = get_checker()
+            assert mocked_checker.has_core_infrastructures_badge() is False
 
-    def test_has_cran_badge(self, mocker: Mocker):
-        pass
+    def test_has_cran_badge(self, mocker):
+        with mocker:
+            mocked_checker = get_checker()
+            assert mocked_checker.has_cran_badge() is False
 
-    def test_has_crates_badge(self, mocker: Mocker):
-        pass
+    def test_has_crates_badge(self, mocker):
+        with mocker:
+            mocked_checker = get_checker()
+            assert mocked_checker.has_crates_badge() is False
 
-    def test_has_license(self, mocker: Mocker):
-        pass
+    def test_has_license(self, mocker):
+        with mocker:
+            mocked_checker = get_checker()
+            assert mocked_checker.has_license() is True
 
-    def test_has_maven_badge(self, mocker: Mocker):
-        pass
+    def test_has_maven_badge(self, mocker):
+        with mocker:
+            mocked_checker = get_checker()
+            assert mocked_checker.has_maven_badge() is False
 
-    def test_has_npm_badge(self, mocker: Mocker):
-        pass
+    def test_has_npm_badge(self, mocker):
+        with mocker:
+            mocked_checker = get_checker()
+            assert mocked_checker.has_npm_badge() is False
 
-    def test_has_open_repository(self, mocker: Mocker):
-        pass
+    def test_has_open_repository(self, mocker):
+        with mocker:
+            mocked_checker = get_checker()
+            assert mocked_checker.has_open_repository() is True
 
-    def test_has_pypi_badge(self, mocker: Mocker):
-        pass
+    def test_has_pypi_badge(self, mocker):
+        with mocker:
+            mocked_checker = get_checker()
+            assert mocked_checker.has_pypi_badge() is False
 
-    def test_has_rsd_badge(self, mocker: Mocker):
-        pass
+    def test_has_rsd_badge(self, mocker):
+        with mocker:
+            mocked_checker = get_checker()
+            assert mocked_checker.has_rsd_badge() is False
 
-    def test_has_zenodo_badge(self, mocker: Mocker):
-        pass
+    def test_has_zenodo_badge(self, mocker):
+        with mocker:
+            mocked_checker = get_checker()
+            assert mocked_checker.has_zenodo_badge() is False
 
-    def test_has_zenodo_metadata_file(self, mocker: Mocker):
-        pass
+    def test_has_zenodo_metadata_file(self, mocker):
+        with mocker:
+            mocked_checker = get_checker()
+            assert mocked_checker.has_zenodo_metadata_file() is False
 
-    def test_is_on_github_marketplace(self, mocker: Mocker):
-        pass
+    def test_is_on_github_marketplace(self, mocker):
+        with mocker:
+            mocked_checker = get_checker()
+            assert mocked_checker.is_on_github_marketplace() is True
 
-    def test_readme(self, mocker: Mocker):
-        pass
+    def test_readme(self, mocker):
+        with mocker:
+            mocked_checker = get_checker()
+            assert isinstance(mocked_checker.readme, Readme)
 
-    def test_repo(self, mocker: Mocker):
-        pass
+    def test_repo(self, mocker):
+        with mocker:
+            mocked_checker = get_checker()
+            assert isinstance(mocked_checker.repo, Repo)
