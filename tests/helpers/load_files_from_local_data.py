4dfd444737ff628b6afd27200e8351f3e992ed09
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2021-02-16 17:53:24 +0100

    As all data files are utf-8 we can directly read them as such

--- tests/helpers/load_files_from_local_data.py
+++ tests/helpers/load_files_from_local_data.py
@@ -4,7 +4,7 @@ import os
 def load_files_from_local_data(who_is_asking_file, dir_type):
     files_dict = dict()
     for key, fullpath in list_files_from_local_data(who_is_asking_file, dir_type).items():
-        with open(fullpath, "rt") as fid:
+        with open(fullpath, "rt", encoding='utf-8') as fid:
             files_dict[key] = fid.read()
 
     return files_dict
