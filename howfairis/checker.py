1b91a2b05c99bc553c92c68bfd51dab760ebc82d
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2021-02-17 16:56:36 +0100

    Revert "Even dryer"
    This reverts commit 93d9050911dfd0a6723d54f942d6d1afd3a7e68f.

--- howfairis/checker.py
+++ howfairis/checker.py
@@ -109,6 +109,7 @@ class Checker(RepositoryMixin, LicenseMixin, RegistryMixin, CitationMixin, Check
             return dict()
 
         raw_url = repo.raw_url_format_string.format(repo_config_filename)
+        non_default_repo_config_filename = repo_config_filename != DEFAULT_CONFIG_FILENAME
 
         try:
             response = requests.get(raw_url)
@@ -117,7 +118,7 @@ class Checker(RepositoryMixin, LicenseMixin, RegistryMixin, CitationMixin, Check
             if non_default_repo_config_filename:
                 print("Using the configuration file {0}".format(raw_url))
         except requests.HTTPError as e:
-            if repo_config_filename != DEFAULT_CONFIG_FILENAME:
+            if non_default_repo_config_filename:
                 raise Exception("Could not find the configuration file {0}".format(raw_url)) from e
             return dict()
 
