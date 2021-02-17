9d0bae525fab1a8df2619e253d666e815dc2bc49
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-02-17 14:40:22 +0100

    replaced get_mocked_repo with get_repo and get_mocked_checker with get_checker; refs #228

--- tests/github/repo1/test_repo_with_branch_tag.py
+++ tests/github/repo1/test_repo_with_branch_tag.py
@@ -3,7 +3,7 @@ from howfairis import Repo
 from tests.contracts.repo import Contract
 
 
-def get_mocked_repo():
+def get_repo():
     return Repo("https://github.com/fair-software/badge", branch="0.1.0")
 
 
@@ -11,46 +11,46 @@ class TestRepoWithBranchTag(Contract):
 
     def test_api(self, mocker):
         with mocker:
-            mocked_repo = get_mocked_repo()
+            mocked_repo = get_repo()
             assert mocked_repo.api == "https://api.github.com/repos/fair-software/badge"
 
     def test_branch(self, mocker):
         with mocker:
-            mocked_repo = get_mocked_repo()
+            mocked_repo = get_repo()
             assert mocked_repo.branch == "0.1.0"
 
     def test_default_branch(self, mocker):
         with mocker:
-            mocked_repo = get_mocked_repo()
+            mocked_repo = get_repo()
             assert mocked_repo.default_branch is None
 
     def test_owner(self, mocker):
         with mocker:
-            mocked_repo = get_mocked_repo()
+            mocked_repo = get_repo()
             assert mocked_repo.owner == "fair-software"
 
     def test_path(self, mocker):
         with mocker:
-            mocked_repo = get_mocked_repo()
+            mocked_repo = get_repo()
             assert mocked_repo.path == ""
 
     def test_platform(self, mocker):
         with mocker:
-            mocked_repo = get_mocked_repo()
+            mocked_repo = get_repo()
             assert mocked_repo.platform == Platform.GITHUB
 
     def test_raw_url_format_string(self, mocker):
         with mocker:
-            mocked_repo = get_mocked_repo()
+            mocked_repo = get_repo()
             assert mocked_repo.raw_url_format_string == "https://raw.githubusercontent.com/fair-software" + \
                                                         "/badge/0.1.0/{0}"
 
     def test_repo(self, mocker):
         with mocker:
-            mocked_repo = get_mocked_repo()
+            mocked_repo = get_repo()
             assert mocked_repo.repo == "badge"
 
     def test_url(self, mocker):
         with mocker:
-            mocked_repo = get_mocked_repo()
+            mocked_repo = get_repo()
             assert mocked_repo.url == "https://github.com/fair-software/badge"
--- tests/gitlab/repo1/test_repo_no_args.py
+++ tests/gitlab/repo1/test_repo_no_args.py
@@ -3,7 +3,7 @@ from howfairis import Repo
 from tests.contracts.repo import Contract
 
 
-def get_mocked_repo():
+def get_repo():
     return Repo("https://gitlab.com/jspaaks/badge-test")
 
 
@@ -11,45 +11,45 @@ class TestRepoNoArgs(Contract):
 
     def test_api(self, mocker):
         with mocker:
-            mocked_repo = get_mocked_repo()
+            mocked_repo = get_repo()
             assert mocked_repo.api == "https://gitlab.com/api/v4/projects/jspaaks%2Fbadge-test"
 
     def test_branch(self, mocker):
         with mocker:
-            mocked_repo = get_mocked_repo()
+            mocked_repo = get_repo()
             assert mocked_repo.branch is None
 
     def test_default_branch(self, mocker):
         with mocker:
-            mocked_repo = get_mocked_repo()
+            mocked_repo = get_repo()
             assert mocked_repo.default_branch == "master"
 
     def test_owner(self, mocker):
         with mocker:
-            mocked_repo = get_mocked_repo()
+            mocked_repo = get_repo()
             assert mocked_repo.owner == "jspaaks"
 
     def test_path(self, mocker):
         with mocker:
-            mocked_repo = get_mocked_repo()
+            mocked_repo = get_repo()
             assert mocked_repo.path == ""
 
     def test_platform(self, mocker):
         with mocker:
-            mocked_repo = get_mocked_repo()
+            mocked_repo = get_repo()
             assert mocked_repo.platform == Platform.GITLAB
 
     def test_raw_url_format_string(self, mocker):
         with mocker:
-            mocked_repo = get_mocked_repo()
+            mocked_repo = get_repo()
             assert mocked_repo.raw_url_format_string == "https://gitlab.com/jspaaks/badge-test/-/raw/master/{0}"
 
     def test_repo(self, mocker):
         with mocker:
-            mocked_repo = get_mocked_repo()
+            mocked_repo = get_repo()
             assert mocked_repo.repo == "badge-test"
 
     def test_url(self, mocker):
         with mocker:
-            mocked_repo = get_mocked_repo()
+            mocked_repo = get_repo()
             assert mocked_repo.url == "https://gitlab.com/jspaaks/badge-test"
