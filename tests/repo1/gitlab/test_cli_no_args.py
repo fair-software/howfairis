72f01fa8fdf264a059e89d38e644f7ce07e7e94f
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-02-19 16:20:56 +0100

    added equivalent of repo1/github to repo1/gitlab

---
+++ tests/repo1/gitlab/test_cli_no_args.py
@@ -0,0 +1,35 @@
+import pytest
+from click.testing import CliRunner
+from howfairis import __version__
+from howfairis.cli.cli import cli
+from tests.contracts.cli import Contract
+from tests.helpers import load_files_from_local_data
+
+
+@pytest.fixture
+def invoke_cli(mocker):
+    runner = CliRunner()
+
+    def _invoker(args):
+        with mocker:
+            return runner.invoke(cli, args, catch_exceptions=False)
+
+    return _invoker
+
+
+class TestCli(Contract):
+
+    def test_show_default_config(self, invoke_cli):
+        result = invoke_cli("--show-default-config")
+        expected = load_files_from_local_data(__file__, "user")["/.howfairis-default.yml"]
+        assert result.stdout == expected
+
+    def test_version_option(self, invoke_cli):
+        result = invoke_cli("--version")
+        assert __version__ in result.stdout
+
+    def test_with_a_url(self, invoke_cli):
+        result = invoke_cli("https://gitlab.com/owner1/repo1")
+        expected = load_files_from_local_data(__file__, "output")["/cli-no-args.txt"]
+        assert result.exit_code == 0
+        assert result.stdout == expected
