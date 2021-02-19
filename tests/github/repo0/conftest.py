75d0a2fe4067a1f43daa05e0b50262d386f925c0
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-02-19 13:25:03 +0100

    clarified the case with varnames, docstring; changed the return code for license

--- tests/github/repo0/conftest.py
+++ tests/github/repo0/conftest.py
@@ -8,11 +8,11 @@ from tests.helpers import get_urls
 @pytest.fixture
 def mocker() -> Mocker:
 
-    """This mock aims to reflect the state of the repository at
-    https://github.com/fair-software/doesnotexist which does not exist"""
+    """This mock aims to reflect the state of a fictious repository at
+    https://github.com/owner-doesnt-exist/repo-doesnt-exist which does not exist"""
 
     with requests_mock.Mocker() as m:
-        repo, raw, api = get_urls(Platform.GITHUB, owner="fair-software", repo="doesnotexist")
+        repo, raw, api = get_urls(Platform.GITHUB, owner="owner-doesnt-exist", repo="repo-doesnt-exist")
         m.get(repo, status_code=404)
         m.get(raw + "/master/.howfairis.yml", status_code=404)
         m.get(raw + "/master/.zenodo.json", status_code=404)
@@ -21,6 +21,6 @@ def mocker() -> Mocker:
         m.get(raw + "/master/CITATION", status_code=404)
         m.get(raw + "/master/README.md", status_code=404)
         m.get(raw + "/master/README.rst", status_code=404)
-        m.get(api + "/license", status_code=200)
+        m.get(api + "/license", status_code=404)
         m.get(api, status_code=404)
         return m
