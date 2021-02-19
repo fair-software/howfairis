95659195bf158879acf4942ae4e191a9f3de06f4
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-02-19 15:53:23 +0100

    filename should be md for consistency

--- tests/test_compliance.py
+++ tests/test_compliance.py
@@ -6,7 +6,7 @@ from howfairis import Compliance
 def test_get_compliance():
     text = "some bogus text here but then we see the badge https://img.shields.io/badge/fair--software.eu-" + \
            "%E2%97%8F%20%20%E2%97%8F%20%20%E2%97%8F%20%20%E2%97%8F%20%20%E2%97%8B-yellow with some trailing stuff."
-    actual_compliance = Readme(filename="README.rst", text=text, file_format=ReadmeFormat.MARKDOWN).get_compliance()
+    actual_compliance = Readme(filename="README.md", text=text, file_format=ReadmeFormat.MARKDOWN).get_compliance()
     expected_compliance = Compliance(repository=True, license_=True, registry=True, citation=True, checklist=False)
     assert actual_compliance == expected_compliance
 
