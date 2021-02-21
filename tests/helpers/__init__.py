af9a94ecdbaeb94f812fefc5645293689086086e
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-02-21 13:28:44 +0100

    added snippets function to public interface of helpers, removed getting unspecified load_files

--- tests/helpers/__init__.py
+++ tests/helpers/__init__.py
@@ -1,6 +1,6 @@
 from .list_files_from_local_data import list_files_from_local_data
-from .load_files_from_local_data import load_files_from_local_data
 from .load_repo_files_from_local_data import load_repo_files_from_local_data
+from .load_snippets_from_local_data import load_snippets_from_local_data
 from .load_user_files_from_local_data import load_user_files_from_local_data
 from .get_urls import get_urls
 from .skip_unreachable import skip_unreachable
@@ -9,8 +9,8 @@ from .skip_unreachable import skip_unreachable
 __all__ = [
     "get_urls",
     "list_files_from_local_data",
-    "load_files_from_local_data",
     "load_repo_files_from_local_data",
+    "load_snippets_from_local_data",
     "load_user_files_from_local_data",
     "skip_unreachable"
 ]
---
+++ tests/helpers/load_snippets_from_local_data.py
@@ -0,0 +1,5 @@
+from .load_files_from_local_data import load_files_from_local_data
+
+
+def load_snippets_from_local_data(who_is_asking_file):
+    return load_files_from_local_data(who_is_asking_file, dir_type="output")
