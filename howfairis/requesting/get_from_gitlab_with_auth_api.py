443201412db874c0853a8358e74500203acaa828
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-02-21 10:23:43 +0100

    added rate limits for api, frontend, raw parts of gitlab service

--- howfairis/requesting/get_from_gitlab_with_auth_api.py
+++ howfairis/requesting/get_from_gitlab_with_auth_api.py
@@ -1,7 +1,13 @@
 import requests
+from backoff import expo
+from backoff import on_exception
+from ratelimit import RateLimitException
+from ratelimit import limits
 
 
 # https://docs.gitlab.com/ee/user/gitlab_com/index.html#gitlabcom-specific-rate-limits
+@on_exception(expo, RateLimitException, max_tries=8)
+@limits(calls=2000, period=60)
 def get_from_gitlab_with_auth_api(url, apikeys):
 
     headers = {
