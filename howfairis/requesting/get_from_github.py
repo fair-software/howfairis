c1f7819840b723c163f0f566a868defa8ed28b13
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-02-25 18:34:32 +0100

    simplified if condition

--- howfairis/requesting/get_from_github.py
+++ howfairis/requesting/get_from_github.py
@@ -4,7 +4,7 @@ from .get_from_github_with_auth import get_from_github_with_auth
 
 def get_from_github(url, url_type, apikeys=None):
 
-    if not (apikeys and apikeys.get("github-key") and apikeys.get("github-user")):
-        return get_from_github_no_auth(url, url_type)
+    if apikeys and apikeys.get("github-key") and apikeys.get("github-user"):
+        return get_from_github_with_auth(url, url_type, apikeys)
 
-    return get_from_github_with_auth(url, url_type, apikeys)
+    return get_from_github_no_auth(url, url_type)
--- howfairis/requesting/get_from_gitlab.py
+++ howfairis/requesting/get_from_gitlab.py
@@ -4,7 +4,7 @@ from .get_from_gitlab_with_auth import get_from_gitlab_with_auth
 
 def get_from_gitlab(url, url_type, apikeys=None):
 
-    if apikeys is None:
-        return get_from_gitlab_no_auth(url, url_type)
+    if apikeys and apikeys.get("gitlab-key") and apikeys.get("gitlab-user"):
+        return get_from_gitlab_with_auth(url, url_type, apikeys)
 
-    return get_from_gitlab_with_auth(url, url_type, apikeys)
+    return get_from_gitlab_no_auth(url, url_type)
