0cd8275ad2639583fb0900778e783185c9816e11
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-02-25 18:30:36 +0100

    Update howfairis/requesting/get_from_github.py
    Co-authored-by: Erik Tjong Kim Sang <erikt@xs4all.nl>

--- howfairis/requesting/get_from_github.py
+++ howfairis/requesting/get_from_github.py
@@ -4,10 +4,7 @@ from .get_from_github_with_auth import get_from_github_with_auth
 
 def get_from_github(url, url_type, apikeys=None):
 
-    github_apikey_is_invalid = apikeys is not None and (apikeys.get("github-key") is None or
-                                                        apikeys.get("github-user") is None)
-
-    if apikeys is None or github_apikey_is_invalid:
+    if not (apikeys and apikeys.get("github-key") and apikeys.get("github-user")):
         return get_from_github_no_auth(url, url_type)
 
     return get_from_github_with_auth(url, url_type, apikeys)
