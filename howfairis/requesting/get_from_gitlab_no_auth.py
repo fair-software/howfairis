daa829dbd3eb0d03ffb68c635056895c3f4f1ecc
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-03-01 20:14:47 +0100

    was running into ratelimits when using mocks, fixed now

--- howfairis/requesting/get_from_gitlab_no_auth.py
+++ howfairis/requesting/get_from_gitlab_no_auth.py
@@ -1,3 +1,4 @@
+import sys
 from backoff import expo
 from backoff import on_exception
 from ratelimit import RateLimitException
@@ -7,9 +8,18 @@ from .get_from_gitlab_no_auth_frontend import get_from_gitlab_no_auth_frontend
 from .get_from_gitlab_no_auth_raw import get_from_gitlab_no_auth_raw
 
 
+def get_calls():
+
+    if "pytest" in sys.modules:
+        # in testing, the api is mocked, so we overrule the rate limit
+        return 1e6
+    # normal operation, 60 calls per hour for unauthenticated github api access
+    return 500
+
+
 # https://docs.gitlab.com/ee/user/gitlab_com/index.html#gitlabcom-specific-rate-limits
 @on_exception(expo, RateLimitException, max_tries=8)
-@limits(calls=500, period=60)
+@limits(calls=get_calls(), period=60)
 def get_from_gitlab_no_auth(url, url_type):
 
     if url_type == "api":
